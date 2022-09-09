63a8fab5b978497d2a32ae8ccb65aef87d22803f
doublesung <jacky88910@yahoo.com.tw>
2022-09-10 01:16:02 +0800

    add page Search.vue

---
+++ src/pages/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-fluid px-3 px-lg-5 pt-7 d-flex flex-column align-items-center" >
+    
+    <h1 class="">與「台灣」相關的照片</h1>
+    <ul class="nav my-4 my-lg-5 position-relative">
+      <li class="nav-item">
+        <a class="nav-link fs-4 text-active" aria-current="page" href="#">相片</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fs-4" href="#">影片</a>
+      </li>
+    </ul>
+    <Photos/>
+  </div>
+</template>
+
+<script>
+import Photos from '../components/Photos.vue'
+
+export default {
+  name: 'Search',
+  components: {
+    Photos
+  }
+}
+</script>
+
+<style scped lang='scss'>
+</style>
