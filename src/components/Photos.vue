08fb21fe46679a1a3bc16dd23c80914e1aa3e5e9
doublesung <jacky88910@yahoo.com.tw>
2022-09-11 01:11:28 +0800

    import Masonry and imagesLoaded to layout

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,38 +1,25 @@
 <template>
   <div class="container-flulid">
-    <div class="row gx-3 gx-sm-4">
-      <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6">
-        <div class="w-100 mb-3 mb-sm-4" v-for="(photoURL, index) in list1" :key="index">
-          <img class="w-100 rounded-4" :src="photoURL" alt="" loading="lazy">
+    <div class="grid clearfix">
+      <!-- 設定元素的 columnWidth -->
+      <div class="grid-sizer w-xxl-25 w-lg-33 w-50"></div>
+      <!-- 元素 width -->
+      <div class="grid-item container-image w-xxl-23 w-lg-31 w-48" 
+      v-for="(photoURL, index) in list1" :key="index">
+        <img class=" mw-100 rounded-4" :src="photoURL" alt="" loading="lazy">
+        <div class="overlay rounded-4 bg-dark"> 123</div>
+        <div class="photographer position-absolute bottom-0 start-0 m-3 text-white d-none d-sm-block">
+          <img src="" alt="">
+          <span>name</span>
         </div>
-      </div>
-      <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6">
-        <div class="container-image w-100 mb-3 mb-sm-4 position-relative" v-for="(photoURL, index) in list1" :key="index">
-          <img class="w-100 rounded-4" :src="photoURL" alt="" loading="lazy">
-          <div class="overlay rounded-4 bg-dark"></div>
-          <div class="photographer position-absolute bottom-0 start-0 m-3 text-white d-none d-sm-block">
-            <img src="" alt="">
-            <span>name</span>
-          </div>
-          <button type="button" class="collection bg-white position-absolute top-0 end-0 m-3 d-flex justify-content-center align-items-center 
-          rounded-4 border-0 d-none d-sm-block">
-            <i class="ri-folder-add-line fw-normal text-primary"></i>
-          </button>
-          <button type="button" class="download bg-white position-absolute bottom-0 end-0 m-3 d-flex justify-content-center align-items-center 
-          rounded-4 border-0 d-none d-sm-block">
-            <i class="ri-download-2-line fw-normal text-primary"></i>
-          </button>
-        </div>
-      </div>
-      <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6">
-        <div class="w-100 mb-3 mb-sm-4" v-for="(photoURL, index) in list1" :key="index">
-          <img class="w-100 rounded-4" :src="photoURL" alt="" loading="lazy">
-        </div>
-      </div>
-      <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6">
-        <div class="w-100 mb-3 mb-sm-4" v-for="(photoURL, index) in list1" :key="index">
-          <img class="w-100 rounded-4" :src="photoURL" alt="" loading="lazy">
-        </div>
+        <button type="button" class="collection bg-white position-absolute top-0 end-0 m-3 d-flex justify-content-center align-items-center 
+        rounded-4 border-0 d-none d-sm-block">
+          <i class="ri-folder-add-line fw-normal text-primary"></i>
+        </button>
+        <button type="button" class="download bg-white position-absolute bottom-0 end-0 m-3 d-flex justify-content-center align-items-center 
+        rounded-4 border-0 d-none d-sm-block">
+          <i class="ri-download-2-line fw-normal text-primary"></i>
+        </button>
       </div>
     </div>
   </div>
@@ -41,6 +28,8 @@
 <script>
 import axios from 'axios'
 import {mapState} from 'vuex'
+import Masonry from 'masonry-layout';
+import imagesLoaded from '../../node_modules/imagesloaded/imagesloaded.pkgd';
 
 export default {
   name: 'Photos',
@@ -59,7 +48,12 @@ export default {
         "http://vcdnb.huoying666.com/images/2021/1212/c3d551f63ba1a0d121dcd51b53932078/c3d551f63b.jpg_home-image-640",
         "https://tour.taitung.gov.tw/image/26556/1024x768",
         "http://vcdnb.huoying666.com/images/2021/1212/c3d551f63ba1a0d121dcd51b53932078/c3d551f63b.jpg_home-image-640",
-      ]
+        "https://tour.taitung.gov.tw/image/26556/1024x768",
+        "http://vcdnb.huoying666.com/images/2021/1212/c3d551f63ba1a0d121dcd51b53932078/c3d551f63b.jpg_home-image-640",
+        "https://tour.taitung.gov.tw/image/26556/1024x768",
+        "http://vcdnb.huoying666.com/images/2021/1212/c3d551f63ba1a0d121dcd51b53932078/c3d551f63b.jpg_home-image-640",
+      ],
+      cool: true
     }
   },
   methods: {
@@ -77,8 +71,20 @@ export default {
     },
   },
   mounted() {
-    this.$store.dispatch('getChosenPhotos')
+    // this.$store.dispatch('getChosenPhotos')
     // this.$store.dispatch('getChosenPhotos', {page: 1})
+    var grid = document.querySelector('.grid');
+
+    var msnry = new Masonry( grid, {
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+    });
+
+    imagesLoaded( grid ).on( 'progress', function() {
+      // 圖片加載完畢後布局
+      msnry.layout();
+    });
   },
 }
 </script>
@@ -88,9 +94,7 @@ export default {
   .overlay {
     position: absolute;
     top: 0;
-    bottom: 0;
     left: 0;
-    right: 0;
     height: 100%;
     width: 100%;
     opacity: 0;
@@ -118,7 +122,12 @@ export default {
       opacity: 1;
     }
   }
+}
 
-
+.grid {
+  margin-right: -2%;
+  .grid-item {
+    margin-bottom: 2%;
+  }
 }
 </style>
